2cd924d288a57115ca8231ca691b0f699865dbc4
Alex Bunting <alex.bunting314@gmail.com>
2024-11-11 10:00:09 +1100

    Add in environment variables files that will be used to configure dev and prod builds of utility app

--- quickshop-utility-app/app/page.tsx
+++ quickshop-utility-app/app/page.tsx
@@ -1,6 +1,18 @@
 import Image from "next/image";
 
-export default function Home() {
+async function fetchData() {
+  console.log("Node.js environment");
+  console.table({
+    QUICKSHOP_SCHEMA: process.env.NEXT_PUBLIC_QUICKSHOP_SCHEME
+  });
+}
+
+export default async function Home() {
+  await fetchData();
+  console.log("Browser environment");
+  console.table({
+    QUICKSHOP_SCHEMA: process.env.NEXT_PUBLIC_QUICKSHOP_SCHEME
+  });
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
@@ -22,7 +34,7 @@ export default function Home() {
           </li>
           <li>Save and see your changes instantly.</li>
         </ol>
-
+        <div>Quickshop schema: {process.env.NEXT_PUBLIC_QUICKSHOP_SCHEME }</div>
         <div className="flex gap-4 items-center flex-col sm:flex-row">
           <a
             className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
