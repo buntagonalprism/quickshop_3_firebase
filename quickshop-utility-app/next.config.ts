d8c9d42e3372e12c9c7009e173b8fcf202e82eba
Alex Bunting <alex.bunting314@gmail.com>
2024-11-13 17:42:13 +1100

    Set images to unoptimised to fix issue with build export

--- quickshop-utility-app/next.config.ts
+++ quickshop-utility-app/next.config.ts
@@ -1,8 +1,13 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
-  output: 'export'
+  /* Output to a static html + js bundle */
+  output: 'export',
+
+  /* Required due to this issue: https://stackoverflow.com/questions/65487914/error-image-optimization-using-next-js-default-loader-is-not-compatible-with-n */
+  images: {
+    unoptimized: true,
+  }
 };
 
 export default nextConfig;
