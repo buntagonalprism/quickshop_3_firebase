bac8690a3a85f8f81637dcd8787f59dc01dc643a
Alex Bunting <alex.bunting314@gmail.com>
2024-11-18 17:30:41 +1100

    Change functions to Sydney location

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,27 +1,19 @@
-/* eslint-disable max-len */
-/**
- * Import function triggers from their respective submodules:
- *
- * import {onCall} from "firebase-functions/v2/https";
- * import {onDocumentWritten} from "firebase-functions/v2/firestore";
- *
- * See a full list of supported triggers at https://firebase.google.com/docs/functions
- */
-
+import {setGlobalOptions} from "firebase-functions/options";
 import {onRequest, Request} from "firebase-functions/v2/https";
 import * as admin from "firebase-admin";
 import * as logger from "firebase-functions/logger";
 import {FieldValue} from "firebase-admin/firestore";
 
 
-// Start writing functions
-// https://firebase.google.com/docs/functions/typescript
+// Set the default execution region for all functions to Sydney
+setGlobalOptions({region: "australia-southeast1"});
 
 admin.initializeApp();
 
 type UserId = string;
+type ErrorMessage = string;
 
-const validateFirestoreToken = async (req: Request): Promise<[UserId | null, string | null]> => {
+const validateFirestoreToken = async (req: Request): Promise<[UserId | null, ErrorMessage | null]> => {
   const authHeader = req.headers.authorization;
   if (!authHeader) {
     return [null, "Authorization header is missing"];
