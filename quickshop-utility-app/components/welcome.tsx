3597a7d06d84887b3d683bc0bf6955cc224ac58c
Alex Bunting <alex.bunting314@gmail.com>
2024-11-22 15:18:24 +1100

    Condense styling of utility app page

--- quickshop-utility-app/components/welcome.tsx
+++ quickshop-utility-app/components/welcome.tsx
@@ -23,8 +23,8 @@ export default function Welcome() {
   console.log("appLink: " + appLink);
 
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center">
+    <div className="flex items-center justify-center min-h-screen p-8 pb-10 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+      <main className="flex flex-col gap-7 row-start-2 items-center">
         <Image
           src="/logo.svg"
           alt="Quickshop logo"
